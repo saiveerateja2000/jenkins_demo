dc498754953444325fb2b00e2725742bfa03b435
saiveerateja2000 <142569965+saiveerateja2000@users.noreply.github.com>
2024-04-05 14:31:37 +0530

    Update unit.sh

--- unit.sh
+++ unit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Hardcoded values
-number=10
 OBJECT=($OBJECTS);
 OBJECT_FIRST_THREE=("${OBJECT[@]:0:3}");
 
@@ -10,14 +9,20 @@ echo $OBJECTS;
 for obj in "${OBJECT[@]}"; do
     echo ${obj};
     python3 -m venv pyvenv/$obj; \
-    . pyvenv/${obj}/bin/activate; \
+    source pyvenv/${obj}/bin/activate; \
     echo $VIRTUAL_ENV
     if [[ " ${OBJECT_FIRST_THREE[@]} " =~ " ${obj} " ]]; then
     echo "$obj is in the first three elements of OBJECT"
     touch pyvenv/${obj}/${obj}_beta.txt
     pip3 install pylint > /dev/null 2>&1
     else
-    exit 1
-    fi
     deactivate
+    echo $VIRTUAL_ENV
+    exit 1
+    echo "hello"
+    fi
 done
+echo $VIRTUAL_ENV
+echo "end of loop"
+deactivate
+echo $VIRTUAL_ENV
